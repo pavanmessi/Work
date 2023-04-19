d7453ba8e78eb0bbeccb198f59f138345be773df
Pavan Kumar <121422699+pavanmessi@users.noreply.github.com>
2023-04-19 21:22:39 +0530

    Create ArraySort.java

---
+++ ArraySort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        int[] hi = {5, 2, 8, 3, 1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(i));
+    }
+}
